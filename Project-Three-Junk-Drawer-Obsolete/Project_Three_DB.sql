3c17e4096d53920f9b11fa195a70b4062d130fca
Austin Silverstein <143352217+silversteinaustin@users.noreply.github.com>
2024-02-08 20:43:07 -0500

    Pushing files to repo

---
+++ Project-Three-Junk-Drawer-Obsolete/Project_Three_DB.sql
@@ -0,0 +1,29 @@
+CREATE TABLE Marriage(
+	state VARCHAR PRIMARY KEY,
+	Year2000 Numeric,
+	Year2004 Numeric,
+	Year2008 Numeric,
+	Year2012 Numeric,
+	Year2016 Numeric,
+	Year2020 Numeric	
+);
+
+CREATE TABLE Divorce(
+	state VARCHAR PRIMARY KEY,
+	Year2000 Numeric,
+	Year2004 Numeric,
+	Year2008 Numeric,
+	Year2012 Numeric,
+	Year2016 Numeric,
+	Year2020 Numeric	
+);
+
+CREATE TABLE Parties(
+		state VARCHAR PRIMARY KEY,
+	Year2000 Numeric,
+	Year2004 Numeric,
+	Year2008 Numeric,
+	Year2012 Numeric,
+	Year2016 Numeric,
+	Year2020 Numeric	
+);
